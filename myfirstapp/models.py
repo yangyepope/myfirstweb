08c99d1c4e500aee7400023150510b24756e4ce7
yangye (H) <y84120554@china.huawei.com>
2020-04-01 20:26:22 +0800

    fu修改

--- myfirstapp/models.py
+++ myfirstapp/models.py
@@ -1,8 +1,9 @@
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+
 
 from django.db import models
 
