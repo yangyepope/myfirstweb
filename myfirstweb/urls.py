1ea8979ac0d2dc1d43c4559fdd9e6c7b55d57d17
yangye (H) <y84120554@china.huawei.com>
2020-04-01 23:10:24 +0800

    kuaji

--- myfirstweb/urls.py
+++ myfirstweb/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
 from django.views.generic import TemplateView
-import myfirstweb.myfirstweb.urls
+import myfirstweb.myfirstapp.urls
 
 
 
