97b608f0a9b5a0eb5db2ac9e9c9987f0e8df6bdf
yangye (H) <y84120554@china.huawei.com>
2020-04-01 20:04:07 +0800

    多个修改

--- myfirstweb/settings.py
+++ myfirstweb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myfirstapp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'myfirstweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myweb',
+        'USER': 'root',
+        'PASSWORD': 'Pope5332941!',
+        'HOST': '114.115.211.30',
     }
 }
 
