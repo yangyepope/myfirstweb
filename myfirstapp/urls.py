041643c91bfe4353856dd89646f6da2ec5581e3b
yangye (H) <y84120554@china.huawei.com>
2020-04-01 22:42:44 +0800

    urls

--- myfirstapp/urls.py
+++ myfirstapp/urls.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls import url
+
 from myfirstweb.myfirstapp import views
 
 
--- myfirstweb/urls.py
+++ myfirstweb/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
 from django.views.generic import TemplateView
+#import myfirstweb.myfirstapp.urls
 from myfirstweb.myfirstapp import urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api/', include(myfirstweb.myfirstapp.urls)),
+    url(r'^api/', include(urls)),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
 ]
